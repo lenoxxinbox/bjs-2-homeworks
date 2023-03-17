c19325cc90d3715cb7b02ef6ff7fe988324c9c19
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-03-17 06:59:51 +0300

    Delete console.log(d);, empty line added

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,8 +1,8 @@
 "use strict";
+
 function solveEquation(a, b, c) {
   let arr = [];
   const d = Math.pow(b, 2) - 4 * a * c;
-  console.log(d);
   if (d < 0) {
     arr = [];
   } else if (d === 0) {
