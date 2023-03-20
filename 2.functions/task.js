8c21bf3d16e3624b14831a96e0b66ce726ad94d3
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-03-20 08:12:04 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,90 @@
 function getArrayParams(...arr) {
+    let min = Infinity;
+    let max = -Infinity;
+    let sum = 0;
 
-  return { min: min, max: max, avg: avg };
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i]
+        };
+        if (arr[i] > max) {
+            max = arr[i]
+        };
+        sum += arr[i];
+        
+    }
+    
+    let avg = parseFloat((sum / arr.length).toFixed(2));
+
+    return { min: min, max: max, avg: avg };
 }
 
 function summElementsWorker(...arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
 
+    return arr.reduce((element, current) => element + current, 0);;
 }
 
 function differenceMaxMinWorker(...arr) {
 
+    if (arr.length === 0) {
+        return 0;
+    }
+
+    let maxElement = Math.max.apply(null, arr);
+    let minElement = Math.min.apply(null, arr);
+
+    return maxElement - minElement;
 }
 
 function differenceEvenOddWorker(...arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
 
+    let sumEvenElement = 0;
+    let sumOddElement = 0;
+
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            sumEvenElement += arr[i];
+        } else {
+            sumOddElement += arr[i];
+        }
+    }
+
+    return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+    if (arr.length === 0) {
+        return 0;
+    }
 
+    let sumEvenElement = 0;
+    let countEvenElement = 0;
+
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] % 2 === 0) {
+            sumEvenElement += arr[i];
+            countEvenElement += 1;
+        }
+    }
+    
+    return sumEvenElement / countEvenElement;
 }
 
 function makeWork (arrOfArr, func) {
+    let maxWorkerResult = func(...arrOfArr[0]);
 
+    for (i = 0; i < arrOfArr.length; i++) {
+
+        const result = func(...arrOfArr[i]);
+        if (result > maxWorkerResult) {
+            maxWorkerResult = result;
+        }
+    }
+    return maxWorkerResult;
 }
