0cf762f1ac0ae3e5244d53dfad57d5ee14c3fe1d
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-03-24 08:00:28 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,33 @@
+"use strict";
+
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    return this.subject = subjectName;
 }
 
-Student.prototype.addMarks = function (...marks) {
-  
+Student.prototype.addMarks = function (...marksToAdd) {
+    if ("marks" in this) {
+        return this.marks.push(...marksToAdd);
+    } else {
+        return 0;
+    }
 }
 
 Student.prototype.getAverage = function () {
-  
+    if ("marks" in this && this.marks.length !== 0) {
+        return this.marks.reduce((item, next) => item + next, 0) / this.marks.length;        
+    }
+    return 0;    
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    delete this.subject;
+    delete this.marks;
+    return this.excluded = reason;
 }
