f4e8ab7477c8c34a286206c348f865a4977bc98f
lenoxxinbox <68026634+lenoxxinbox@users.noreply.github.com>
2023-04-01 21:16:35 +0300

    Update task.js

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -1,12 +1,12 @@
 "use strict";
 
 class PrintEditionItem {
-  constructor(name, releaseDate, pagesCount, state, type = null) {
+  constructor(name, releaseDate, pagesCount) {
     this.name = name;
     this.releaseDate = releaseDate;
     this.pagesCount = pagesCount;
     this.state = 100;
-    this.type = type;
+    this.type = null;
   }
 
   fix() {
@@ -31,39 +31,39 @@ class PrintEditionItem {
 }
 
 class Magazine extends PrintEditionItem {
-  constructor(name, releaseDate, pagesCount, state) {
-    super(name, releaseDate, pagesCount, state);
+  constructor(name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
     this.type = "magazine";
   }
 }
 
 class Book extends PrintEditionItem {
-  constructor(author, name, releaseDate, pagesCount, state) {
-    super(name, releaseDate, pagesCount, state);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(name, releaseDate, pagesCount);
     this.type = "book";
     this.author = author;
   }
 }
 
 class NovelBook extends Book {
-  constructor(author, name, releaseDate, pagesCount, state) {
-    super(author, name, releaseDate, pagesCount, state);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = "novel";
     this.author = author;
   }
 }
 
 class FantasticBook extends Book {
-  constructor(author, name, releaseDate, pagesCount, state) {
-    super(author, name, releaseDate, pagesCount, state);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = "fantastic";
     this.author = author;
   }
 }
 
 class DetectiveBook extends Book {
-  constructor(author, name, releaseDate, pagesCount, state) {
-    super(author, name, releaseDate, pagesCount, state);
+  constructor(author, name, releaseDate, pagesCount) {
+    super(author, name, releaseDate, pagesCount);
     this.type = "detective";
     this.author = author;
   }
